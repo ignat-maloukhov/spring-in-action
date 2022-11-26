c9193301188e2fdb7218faf483e76ee9deb342f5
Ignat Maloukhov <ignatmaloukhov@gmail.com>
2022-11-26 14:02:15 +0300

    Add HomeController, home.html, TacoApplicationTest. First chapter is completed.

---
+++ src/test/java/edu/ignat/springinaction/TacoApplicationTest.java
@@ -0,0 +1,32 @@
+package edu.ignat.springinaction;
+
+
+import edu.ignat.springinaction.controller.HomeController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(HomeController.class)
+public class TacoApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Successful HomeController test")
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to Taco Cloud")));
+    }
+
+}
